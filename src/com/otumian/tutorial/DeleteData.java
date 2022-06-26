828d4f5f1c887421db07ae0d288d0c83019c8142
otumian <popecan1000@gmail.com>
2022-06-26 17:44:34 +0000

    DeleteData.java: delete rows from table

---
+++ src/com/otumian/tutorial/DeleteData.java
@@ -0,0 +1,42 @@
+package com.otumian.tutorial;
+
+import com.otumian.util.DatabaseConnection;
+import com.otumian.util.DatabasePath;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DeleteData {
+    public DeleteData() {
+
+    }
+
+    public static void main(String[] args) {
+        DeleteData data = new DeleteData();
+
+        data.deleteTask(3);
+
+        data.deleteTask(4);
+
+        data.deleteTask(5);
+    }
+
+    void deleteTask(int id) {
+        String url = DatabasePath.url("todoapp.db");
+        Connection connection = DatabaseConnection.connection(url);
+
+        String sql = "DELETE FROM task WHERE id=?";
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, id);
+            statement.executeUpdate();
+
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            // throw new RuntimeException(e);
+        }
+    }
+}
--- src/com/otumian/tutorial/notes.md
+++ src/com/otumian/tutorial/notes.md
@@ -5,3 +5,4 @@
 - InsertData
 - SelectData
 - UpdateData
+- DeleteData
