b7cf18a0aa0bcdbe21e2a053ee7f18c1f77369c0
otumian <popecan1000@gmail.com>
2022-06-26 14:38:45 +0000

    CreateTable.java: Create a database table

---
+++ src/com/otumian/tutorial/CreateTable.java
@@ -0,0 +1,41 @@
+package com.otumian.tutorial;
+
+import com.otumian.util.DatabasePath;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CreateTable {
+    public static void main(String[] args) {
+
+        String url = DatabasePath.url("todoapp.db");
+
+        String sql = "CREATE TABLE IF NOT EXISTS \"task\" (";
+        sql += "\"id\" INTEGER,";
+        sql += "\"title\" TEXT NOT NULL,";
+        sql += "\"description\"	TEXT,";
+        sql += "PRIMARY KEY(\"id\" AUTOINCREMENT));";
+
+        try {
+            Connection conn = DriverManager.getConnection(url);
+
+            if (conn != null) {
+                Statement statement = conn.createStatement();
+
+                statement.execute(sql);
+                conn.commit();
+                conn.close();
+
+            } else {
+                throw new SQLException("Connection was null");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+}
