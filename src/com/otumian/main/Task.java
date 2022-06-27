e24ac50f880ae728c400014dbbe2fd5ff2ce1ed6
otumian <popecan1000@gmail.com>
2022-06-27 01:06:00 +0000

    Create Task Model

---
+++ src/com/otumian/main/Task.java
@@ -0,0 +1,40 @@
+package com.otumian.main;
+
+public class Task {
+    private int id = 0;
+    private String title = "";
+    private String description ="";
+
+    public Task() {
+    }
+
+    public Task(int id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
